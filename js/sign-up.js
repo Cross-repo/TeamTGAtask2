cbd93a2e3fe97e234db14902f59cf3026d0fdd2a
adedejiabiola <adedejiabiola56@gmail.com>
2019-09-18 00:19:47 +0100

    sign-up fixes

--- js/sign-up.js
+++ js/sign-up.js
@@ -3,33 +3,34 @@ const username = document.querySelector(".username-text");
 const email = document.querySelector(".email")
 const password = document.querySelector(".password-text");
 
-function validateUserName(target, language) {
+function validateFullname(target, language) {
   let field = target.value;
-  // CHeck if email
-  if (/\@/.test(field)) {
-      let field = target.value;
-
-      // Validate email address
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field)) {
-        document.getElementById('email-error').style.display = "none"; 
-        return (true);
-      }
-      emailError(language); 
-      document.getElementById('email-error').style.display = "block";
-      target.focus();
-      return (false);
+  
+  // Validate username
+  if (field.length === 0) {
+    target.focus();
+    fullnameErrorMessage(language);
+    document.querySelector('#name-error').style.visibility = "visible";
+    return false;
   } else {
-    // Validate username
-    if (field.length === 0) {
-      target.focus();
-      usernameErrorMessage(language);
-      document.getElementById('name-error').style.display = "block";
-      return false;
-    } else {
-      document.getElementById('name-error').style.display = "none"; 
-      return true;
-    }
+    document.querySelector('#name-error').style.visibility = "hidden"; 
+    return true;
   }
+}
+
+const validateEmail = (target, language) => {
+  let field = target.value;
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(field)) {
+    document.querySelector('#email-error').style.visibility = "hidden"; 
+    return (true);
+  } else {
+    emailErrorMessage(language); 
+    document.querySelector('#email-error').style.visibility = "visible";
+    target.focus();
+    return (false);
+  }
+  
+
 }
 
 
@@ -40,33 +41,38 @@ const validatePassword = (target, language) => {
   if (value.length < 6) {
     target.focus();
     passwordError(language); 
-    document.getElementById('password-invalid').style.display = "block";
+    document.querySelector('#password-error').style.visibility = "visible";
     return false;
   } else {
-    document.getElementById('password-invalid').style.display = "none";
+    document.querySelector('#password-error').style.visibility = "hidden";
     return true;
   }
 }
 
 const fullnameErrorMessage = (language) => {
   // This houses the default error message if user doesnt click on any link.
-  let errorDisplay = document.getElementById('username-text');
+  let errorDisplay = document.getElementById('name-error');
   switch (language) {
+    case "isSpanish":
+        errorDisplay.textContent="Por favor ingrese su nombre completo";
+        break;
+    case 'isFrench':
+        errorDisplay.textContent = "S'il vous plaît remplir votre nom complet";
+        break;
+      case 'isGerman':
+        errorDisplay.textContent = "Bitte geben Sie Ihren vollständigen Namen an";
+        break;
+      case 'isChinese':
+        errorDisplay.textContent = "请填写您的全名";
+        break;
     default:
         errorDisplay.textContent = "Please fill in your fullname";
   }
 }
 
-const emailErrorMessage = (language) => {
-  // This houses the default error message if user doesnt click on any link.
-  let errorDisplay = document.getElementById('username-text');
-  switch (language) {
-    default:
-        errorDisplay.textContent = "You have entered an invalid email address";
-  }
-}
 
-const emailError = (language) => {
+
+const emailErrorMessage = (language) => {
   // This houses the error messages for wrong email address.
   let errorDisplay = document.getElementById('email-error');
   switch (language) {
@@ -94,8 +100,20 @@ const passwordError = (language) => {
   // This houses the default error message for short password if the user does not click on any link.
   let errorDisplay = document.getElementById('password-error');
   switch (language) {
+    case 'isSpanish':
+      errorDisplay.textContent = "Su contraseña debe tener al menos seis caracteres de longitud";
+      break;
+    case 'isFrench':
+      errorDisplay.textContent = "Votre mot de passe doit comporter au moins six caractères";
+      break;
+    case 'isGerman':
+      errorDisplay.textContent = "Ihr Passwort muss mindestens sechs Zeichen lang sein";
+      break;
+    case 'isChinese':
+      errorDisplay.textContent = "您的密码长度必须至少为六个字符";
+      break;
     default:
-        errorDisplay.textContent = "Your password must be at least six characters long.";
+        errorDisplay.textContent = "Your password must be at least six characters long";
   }
 }
 
@@ -112,8 +130,8 @@ const getLanguage = () => {
   langArray.forEach((lang) => {
 
     lang.addEventListener('click', () => {
-      const displayError = document.querySelector('name-error');
-      const emailError = document.querySelector('email-error');
+      const displayError = document.querySelector('#name-error');
+      const emailError = document.querySelector('#email-error');
       let passError = document.getElementById('password-error');
       if (event.target === langArray[0]) {
         displayError.textContent = "Please fill in your username.";
@@ -154,32 +172,58 @@ const getLanguage = () => {
 
 const englishLanguage = () => {
   // English lang text content
-  username.textContent = "Username or email address";
-  password.textContent = "Password";
+  document.querySelector('.username-text').placeholder = "Fullname";
+  document.querySelector('.email').placeholder = "Email Address";
+  document.querySelector('.password-text').placeholder = "Password";
+  document.querySelector('button').textContent = "Sign Up";
+  document.querySelector('h2').textContent= "SIGN UP DETAILS";
+  document.querySelector(".left-float").textContent="Already have an account?";
+  document.querySelector(".left-p").textContent="Open an account with us";
 }
 
 const spanishLanguage = () => {
   //Spanish text content
-  username.textContent = "Nombre de usuario o dirección de correo electrónico";
-  password.textContent = "Contraseña";
+  document.querySelector('.username-text').placeholder = "Nombre completo";
+  document.querySelector('.email').placeholder = "Dirección de correo electrónico";
+  document.querySelector('.password-text').placeholder = "Contraseña";
+  document.querySelector('button').textContent = "Regístrate";
+  document.querySelector('h2').textContent= "REGISTRO DETALLES";
+  document.querySelector(".left-float").textContent="Ya tienes una cuenta?";
+  document.querySelector(".left-p").textContent="Abre una cuenta con nosotros";
+
 }
 
 const frenchLanguage = () => {
   // french text content
-  username.textContent = "Nom d'utilisateur ou adresse e-mail";
-  password.textContent = "Mot de passe";
+  document.querySelector('.username-text').placeholder = "Nom complet";
+  document.querySelector('.email').placeholder = "Adresse e-mail";
+  document.querySelector('.password-text').placeholder = "Mot de passe";
+  document.querySelector('button').textContent = "S'inscrire";
+  document.querySelector('h2').textContent= "INSCRIVEZ-VOUS DÉTAILS";
+  document.querySelector(".left-float").textContent="Vous avez déjà un compte?";
+  document.querySelector(".left-p").textContent="Ouvrir un compte avec nous";
 }
 
 const germanLanguage = () => {
   // german text content
-  username.textContent = "Benutzername oder E-mail Adresse";
-  password.textContent = "Passwort";
+  document.querySelector('.username-text').placeholder = "Vollständiger Name";
+  document.querySelector('.email').placeholder = "E-Mail-Addresse";
+  document.querySelector('.password-text').placeholder = "Passwort";
+  document.querySelector('button').textContent = "Anmelden";
+  document.querySelector('h2').textContent= "REGISTRIEREN SIE SICH DETAILS";
+  document.querySelector(".left-float").textContent="Hast du schon ein Konto?";
+  document.querySelector(".left-p").textContent="Eröffnen Sie ein Konto bei uns";
 }
 
 const chineseLanguage = () => {
   // chinese text content 
-  username.textContent = "用户名或邮箱地址";
-  password.textContent = "密码";
+  document.querySelector('.username-text').placeholder = "全名";
+  document.querySelector('.email').placeholder = "电子邮件地址";
+  document.querySelector('.password-text').placeholder = "密码";
+  document.querySelector('button').textContent = "注册";
+  document.querySelector('h2').textContent= "注册详细信息";
+  document.querySelector(".left-float").textContent="已经有账号？";
+  document.querySelector(".left-p").textContent="与我们开立账户";
 }
 
 
@@ -187,14 +231,16 @@ const uiCanInteract = (language) => {
   // greabs the form fields from the UI and calls the appropriate functions.
   
 
-  const inputArray = [username, password];
+  const inputArray = [username,email, password];
   inputArray.forEach((input) => {
     input.addEventListener('blur', () => {
       if (event.target === inputArray[0]) {
-        validateUsername(event.target, language)
+        validateFullname(event.target, language)
       } else if (event.target === inputArray[1]) {
+        validateEmail(event.target, language);
+      } else if (event.target === inputArray[2]) {
         validatePassword(event.target, language);
-      } 
+      }
     } )
   })
 }
